8af118920ce6bc3353d5bc064c63281e79cd2d44
Sadid Rafan <62620773+nirvik-alpha@users.noreply.github.com>
2023-05-27 03:31:08 +0600

    case  -when  -then -else -end

---
+++ Confirmation Rate.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+
+select s.user_id , round (sum( case when action ='confirmed' then 1 else 0 end)/count(1),2) as confirmation_rate from Signups s left join Confirmations c on s.user_id=c.user_id group by user_id;
+
+
+
